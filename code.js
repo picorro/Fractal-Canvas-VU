e619e3fd3d3e81ecf415a4d6f0d1ae4277dd1b88
Dalius Budrys <dalius.b2011@gmail.com>
2020-10-18 18:55:19 +0300

    Completed
    Made transformation animations

--- code.js
+++ code.js
@@ -75,19 +75,8 @@ function draw(step) {
 
 // Animations
 
-currentScaleX = 1.0;
-currentScaleY = 1.0;
 frameDelay = 10;
-var scaleReductionModifierX = 0.99;
-var scaleReductionModifierY = 0.99;
-function resetScale()
-{
-	var canvas = document.getElementById('myCanvas2');
-    var ctx = canvas.getContext("2d");
-    ctx.scale(1 / currentScaleX, 1 / currentScaleY);
-    currentScaleX = 1;
-    currentScaleY = 1;
-}
+
 function clearCanvas2()
 {
 	var canvas = document.getElementById('myCanvas2');
@@ -114,11 +103,14 @@ function firstStep()
     var canvas = document.getElementById('myCanvas2');
     if (canvas.getContext)
        	var ctx = canvas.getContext("2d");
-	var dx = 125, dy = 125;
+    clearCanvas2();
+	var dx = 250, dy = 250;
 	var i, r, b, red, blue, t = 0, N = 100;
 	var i = 0;
-	setTimeout(animate, frameDelay);
-	function animate2()
+	ctx.save();   
+	ctx.translate(dx,dy);
+	animate();
+	function animate()
 	{
 		if(i < N + 1)
 		{
@@ -128,32 +120,10 @@ function firstStep()
 			anim_L(t);
 			t += 1/N;
 			i++;
-			setTimeout(animate2, frameDelay);
-		}
-		else
-			ctx.translate(-125, -125);
-	}
-	function animate()
-	{
-		ctx.clearRect(0, 0, canvas.width + 1000, canvas.height + 1000);
-		ctx.fillStyle = "black";
-		if(currentScaleX > 0.25)
-		{
-		 	ctx.scale(scaleReductionModifierX, 1);
-		 	ctx.scale(1, scaleReductionModifierY);
-		 	currentScaleX = currentScaleX * scaleReductionModifierX;
-		 	currentScaleY = currentScaleY * scaleReductionModifierY;
-		 	needNextFrame = true;
-		 	fillCanvas();
 			setTimeout(animate, frameDelay);
 		}
 		else
-		{
-			resetScale();
-			ctx.save();           
-			ctx.translate(dx,dy);
-			animate2();
-		}
+			ctx.translate(-250, -250);
 	}
     function draw_L()
     {
@@ -162,20 +132,20 @@ function firstStep()
 	    ctx.translate(-250,-250);
 	    ctx.beginPath();
 	    ctx.moveTo(0,0);
-	    ctx.lineTo(250,0);
-	    ctx.lineTo(250,250);
-	    ctx.lineTo(187.5,250);
-	    ctx.lineTo(187.5,125);
-	    ctx.lineTo(0,125);
+	    ctx.lineTo(250 * 2,0);
+	    ctx.lineTo(250 * 2,250 * 2);
+	    ctx.lineTo(187.5 * 2,250 * 2);
+	    ctx.lineTo(187.5 * 2,125 * 2);
+	    ctx.lineTo(0,125 * 2);
 	    ctx.lineTo(0,0);
 	    ctx.fill();
 	    ctx.restore();
    }
    function anim_L(t) {
      ctx.save();
-     ctx.translate(0*t,0*t);
-	 ctx.rotate(Math.PI*t/2);
-	 ctx.scale(1/2-t,1/2);
+     ctx.translate(-62.5 * t,-62.5 * t);
+	 ctx.rotate(Math.PI * t / 2);
+     ctx.scale((1 - 0.25 * t) - t, 1 - 0.75 * t);
 	 draw_L();
      ctx.restore();   
    }
@@ -187,11 +157,13 @@ function secondStep()
     if (canvas.getContext)
        	var ctx = canvas.getContext("2d");
     clearCanvas2();
-	var dx = 125, dy = 125;
+	var dx = 250, dy = 250;
 	var i, r, b, red, blue, t = 0, N = 100;
 	var i = 0;
-	setTimeout(animate, frameDelay);
-	function animate2()
+	ctx.save();   
+	ctx.translate(dx,dy);
+	animate();
+	function animate()
 	{
 		if(i < N + 1)
 		{
@@ -201,32 +173,10 @@ function secondStep()
 			anim_L(t);
 			t += 1/N;
 			i++;
-			setTimeout(animate2, frameDelay);
-		}
-		else
-			ctx.translate(-125, -125);
-	}
-	function animate()
-	{
-		ctx.clearRect(0, 0, canvas.width + 1000, canvas.height + 1000);
-		ctx.fillStyle = "black";
-		if(currentScaleX > 0.5)
-		{
-		 	ctx.scale(scaleReductionModifierX, 1);
-		 	ctx.scale(1, scaleReductionModifierY);
-		 	currentScaleX = currentScaleX * scaleReductionModifierX;
-		 	currentScaleY = currentScaleY * scaleReductionModifierY;
-		 	needNextFrame = true;
-		 	fillCanvas();
 			setTimeout(animate, frameDelay);
 		}
 		else
-		{
-			resetScale();
-			ctx.save();           
-			ctx.translate(dx,dy);
-			animate2();
-		}
+			ctx.translate(-250, -250);
 	}
     function draw_L()
     {
@@ -246,9 +196,9 @@ function secondStep()
    }
    function anim_L(t) {
      ctx.save();
-     ctx.translate(250*t,0*t);
+     ctx.translate(125 * t,-125*t);
 	 ctx.rotate(Math.PI*t/2);
-	 ctx.scale(1/2,1/2);
+     ctx.scale(1 - 0.5 * t, 1 - 0.5 * t);
 	 draw_L();
      ctx.restore();   
    }
@@ -263,8 +213,10 @@ function thirdStep()
 	var dx = 125, dy = 125;
 	var i, r, b, red, blue, t = 0, N = 100;
 	var i = 0;
-	setTimeout(animate, frameDelay);
-	function animate2()
+	ctx.save();   
+	ctx.translate(dx,dy);
+	animate();
+	function animate()
 	{
 		if(i < N + 1)
 		{
@@ -274,32 +226,10 @@ function thirdStep()
 			anim_L(t);
 			t += 1/N;
 			i++;
-			setTimeout(animate2, frameDelay);
-		}
-		else
-			ctx.translate(-125, -125);
-	}
-	function animate()
-	{
-		ctx.clearRect(0, 0, canvas.width + 1000, canvas.height + 1000);
-		ctx.fillStyle = "black";
-		if(currentScaleX > 0.5)
-		{
-		 	ctx.scale(scaleReductionModifierX, 1);
-		 	ctx.scale(1, scaleReductionModifierY);
-		 	currentScaleX = currentScaleX * scaleReductionModifierX;
-		 	currentScaleY = currentScaleY * scaleReductionModifierY;
-		 	needNextFrame = true;
-		 	fillCanvas();
 			setTimeout(animate, frameDelay);
 		}
 		else
-		{
-			resetScale();
-			ctx.save();           
-			ctx.translate(dx,dy);
-			animate2();
-		}
+			ctx.translate(-125, -125);
 	}
     function draw_L()
     {
@@ -307,35 +237,36 @@ function thirdStep()
 	    ctx.save();
 	    ctx.translate(-250,-250);
 	    ctx.beginPath();
-	    ctx.moveTo(0,0);
-	    ctx.lineTo(250*2,0);
-	    ctx.lineTo(250*2,250*2);
-	    ctx.lineTo(187.5*2,250*2);
-	    ctx.lineTo(187.5*2,125*2);
-	    ctx.lineTo(0,125*2);
-	    ctx.lineTo(0,0);
-	    ctx.fill();
+	   	ctx.moveTo(0,0);
+    	ctx.lineTo(500,0);
+	    ctx.lineTo(500, 500);
+	    ctx.lineTo(187.5 * 2,500 );
+	    ctx.lineTo(187.5 * 2, 125 * 2);
+	    ctx.lineTo(0, 125 * 2);
+    	ctx.lineTo(0,0);
+    	ctx.fill();
 	    ctx.restore();
    }
    function anim_L(t) {
      ctx.save();
-     ctx.translate(250*t,250*t);
-	 ctx.scale(1/2,1/2-t);
+     ctx.translate(250 * t, 250 * t);
+	 ctx.scale(1 - 0.5 * t, (1 - 0.5 * t) - t);
 	 draw_L();
      ctx.restore();   
    }
 }
 function fourthStep()
 {
-     var canvas = document.getElementById('myCanvas2');
+    var canvas = document.getElementById('myCanvas2');
     if (canvas.getContext)
        	var ctx = canvas.getContext("2d");
-    clearCanvas2();
-	var dx = 125, dy = 125;
+	var dx = 250, dy = 250;
 	var i, r, b, red, blue, t = 0, N = 100;
 	var i = 0;
-	setTimeout(animate, frameDelay);
-	function animate2()
+	ctx.save();   
+	ctx.translate(dx,dy);
+	animate();
+	function animate()
 	{
 		if(i < N + 1)
 		{
@@ -347,31 +278,11 @@ function fourthStep()
 			anim_L(t);
 			t += 1/N;
 			i++;
-			setTimeout(animate2, frameDelay);
-		}
-		else
-			ctx.translate(-125, -125);
-	}
-	function animate()
-	{
-		ctx.clearRect(0, 0, canvas.width + 1000, canvas.height + 1000);
-		ctx.fillStyle = "black";
-		if(currentScaleX > 0.5)
-		{
-		 	ctx.scale(scaleReductionModifierX, 1);
-		 	ctx.scale(1, scaleReductionModifierY);
-		 	currentScaleX = currentScaleX * scaleReductionModifierX;
-		 	currentScaleY = currentScaleY * scaleReductionModifierY;
-		 	needNextFrame = true;
-		 	fillCanvas();
 			setTimeout(animate, frameDelay);
 		}
 		else
 		{
-			resetScale();
-			ctx.save();           
-			ctx.translate(dx,dy);
-			animate2();
+			ctx.translate(-250, -250);
 		}
 	}
     function draw_L()
@@ -381,20 +292,19 @@ function fourthStep()
 	    ctx.translate(-250,-250);
 	    ctx.beginPath();
 	    ctx.moveTo(0,0);
-	    ctx.lineTo(250*2,0);
-	    ctx.lineTo(250*2,250*2);
-	    ctx.lineTo(187.5*2,250*2);
-	    ctx.lineTo(187.5*2,125*2);
-	    ctx.lineTo(0,125*2);
+	    ctx.lineTo(500,0);
+	    ctx.lineTo(500, 500);
+	    ctx.lineTo(187.5 * 2,500 );
+	    ctx.lineTo(187.5 * 2, 125 * 2);
+	    ctx.lineTo(0, 125 * 2);
 	    ctx.lineTo(0,0);
 	    ctx.fill();
 	    ctx.restore();
    }
    function anim_L(t) {
      ctx.save();
-     ctx.translate(0*t,250*t);
-	 //ctx.rotate(Math.PI*t/2);
-	 ctx.scale(1/2,1/2);
+     ctx.translate(-125 * t, 125 * t);
+     ctx.scale(1 - 0.5 * t, 1 - 0.5 * t);
 	 draw_L();
      ctx.restore();   
    }
